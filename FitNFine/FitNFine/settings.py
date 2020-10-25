60bd0102460d35f3ef2f37fb700a9d34781695bf
Lakshay <lakshaydhingra32969@gmail.com>
2020-10-25 23:01:15 +0530

    created main_app

--- FitNFine/FitNFine/settings.py
+++ FitNFine/FitNFine/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app.apps.MainAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ FitNFine/main_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# render syntax:
+# return render(request,'page.html',context_var_dictionary)
+
+# Create your views here.
+def index(request):
+    return render(request,'main_app/index.html')
